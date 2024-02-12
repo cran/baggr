230bff3e961f806a13617cd0988ebf58d1fce749
Witold Wiecek <witold.wiecek@gmail.com>
2024-02-12 18:20:02 +0000

    version 0.7.8

--- inst/doc/baggr.R
+++ inst/doc/baggr.R
@@ -51,28 +51,28 @@ effect_draw(baggr_schools, draws = 1)
 ## ----echo=TRUE, eval=FALSE----------------------------------------------------
 #  my_baggr_comparison <- baggr_compare(schools)
 
-## ---- echo=FALSE, eval = TRUE, include = FALSE--------------------------------
+## ----echo=FALSE, eval = TRUE, include = FALSE---------------------------------
 my_baggr_comparison <- baggr_compare(schools)
 
 ## ----fig.width=5, fig.height=4, echo = TRUE-----------------------------------
 plot(my_baggr_comparison) + 
   ggtitle("8 schools: model comparison")
 
-## ---- eval=F, echo=T----------------------------------------------------------
+## ----eval=F, echo=T-----------------------------------------------------------
 #  baggr_schools_v2 <- baggr(schools, prior_hypermean = normal(10, 2.5))
 
-## ---- eval=T, include=F-------------------------------------------------------
+## ----eval=T, include=F--------------------------------------------------------
 baggr_schools_v2 <- baggr(schools, prior_hypermean = normal(10, 2.5))
 
-## ---- fig.width=6, fig.height=5-----------------------------------------------
+## ----fig.width=6, fig.height=5------------------------------------------------
 effect_plot("Default model" = baggr_schools, "normal(10, 2.5)" = baggr_schools_v2) +
   coord_cartesian(xlim = c(-10, 30)) + theme(legend.position = "top")
 baggr_compare("Default model" = baggr_schools, "normal(10, 2.5)" = baggr_schools_v2)
 
-## ---- fig.width=5-------------------------------------------------------------
+## ----fig.width=5--------------------------------------------------------------
 forest_plot(baggr_schools)
 
-## ---- fig.width=5-------------------------------------------------------------
+## ----fig.width=5--------------------------------------------------------------
 forest_plot(baggr_schools, show = "both")
 
 ## ----loocv, echo = T, results = 'hide', warning = F, message = F--------------
@@ -87,7 +87,7 @@ loocv_res
 names(attributes(loocv_res))
 attr(loocv_res, "df")
 
-## ---- echo = FALSE, include = FALSE, results = 'hide'-------------------------
+## ----echo = FALSE, include = FALSE, results = 'hide'--------------------------
 fit1 <- baggr(data = schools[1:7,], test_data = schools[8,], 
               model = "rubin", pooling = "partial")
 fit2 <- baggr(data = schools[1:7,], test_data = schools[8,], 
